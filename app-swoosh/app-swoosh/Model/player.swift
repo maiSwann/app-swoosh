3b386dafff8f8f29b36c05d7fd2654401c596c58
mailys <mailys.perez@epitech.eu>
2020-06-23 09:48:58 +0200

    [ADD] player Model and IBactions for selected league buttons

---
+++ app-swoosh/app-swoosh/Model/player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  app-swoosh
+//
+//  Created by Maïlys Perez on 23/06/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
